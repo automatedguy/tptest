71ff05d42cf3911410aab6e7b6a6c7c5fd5e26f3
gabi <automationthings@gmail.com>
2018-04-08 16:04:39 -0300

    Excersise 1 is working will do some additional fixes

--- base/json_read.py
+++ base/json_read.py
@@ -44,13 +44,21 @@ class JsonReader(object):
                 self.logger.info(TYPE_ERROR)
 
     def iterate_json_two(self, element_id):
-        for element in self.json_file['__FINDERS__']:
-            try:
-                if element['elementID'] == element_id:
-                    self.logger.info(ADD_ELEMENT + ' :[' + str(element))
-                    self.element_list.append(element)
-            except TypeError:
-                self.logger.info(TYPE_ERROR)
+        try:
+            for element in self.json_file['__FINDERS__']:
+                try:
+                    if element['elementID'] == element_id:
+                        self.logger.info(ADD_ELEMENT + ' :[' + str(element))
+                        self.element_list.append(element)
+                except TypeError:
+                    self.logger.info(TYPE_ERROR)
+        except KeyError:
+            self.logger.info(KEY_ERROR)
+
+        import_files_list = self.get_import_files_list()
+
+        for import_file in import_files_list:
+            self.populate_element_list(import_file, element_id)
 
     def populate_element_list(self, import_file, element_id):
         self.json_file = self.load_json(import_file)
@@ -59,8 +67,9 @@ class JsonReader(object):
 
     def get_elements(self, element_id):
         self.json_file = self.load_json(self._FIRST_FILE_)
-        import_files_list = self.get_import_files_list()
         self.iterate_json_one(element_id)
+
+        import_files_list = self.get_import_files_list()
 
         for import_file in import_files_list:
             self.populate_element_list(import_file, element_id)
